ff9cb8b94d1782ad2d089e6d625d40c4b511f8ce
Nick <605820993@qq.com>
2017-01-20 12:02:49 +0800

    random

--- tools/randomGenerator.js
+++ tools/randomGenerator.js
@@ -1,7 +1,7 @@
 "use strict";
 function randomGenerator(len) {
     if (len === void 0) { len = 4; }
-    return "Math.round(1000 + 9000 * Math.random())";
+    return "" + Math.round(1000 + 9000 * Math.random());
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = randomGenerator;
--- tools/randomGenerator.ts
+++ tools/randomGenerator.ts
@@ -1,5 +1,5 @@
 import crypto = require('crypto')
 
 export default function randomGenerator(len = 4) {
-    return `Math.round(1000 + 9000 * Math.random())`
+    return `${Math.round(1000 + 9000 * Math.random())}`
 }
