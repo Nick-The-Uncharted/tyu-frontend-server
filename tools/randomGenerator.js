487675dc60025dd139d0160458ca082e66ad3120
Nick <605820993@qq.com>
2017-01-20 11:57:27 +0800

    random to number

--- tools/randomGenerator.js
+++ tools/randomGenerator.js
@@ -1,8 +1,7 @@
 "use strict";
-var crypto = require("crypto");
 function randomGenerator(len) {
     if (len === void 0) { len = 4; }
-    return crypto.randomBytes(Math.ceil(len * 3 / 4)).toString('base64').slice(0, len).replace(/\+|\//g, "C");
+    return "Math.round(1000 + 9000 * Math.random())";
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = randomGenerator;
