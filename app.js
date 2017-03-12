a3ccc0add5d14ac5fc1ea60d696054ef629f436d
Nick <605820993@qq.com>
2017-03-12 16:01:26 +0800

    意外 意外

--- app.js
+++ app.js
@@ -76,8 +76,15 @@ app.use(proxy('/service', {
     target: config.serverUrl,
     changeOrigin: true,
     pathRewrite: function (path, req) {
-        if (path.indexOf('/serivce') == 0) {
-            return path.substr(8) + "&openID=" + req.session["openid"];
+        if (path.indexOf('/service') == 0) {
+            console.log('path is ' + path);
+            if (path.indexOf('?') >= 0) {
+                return path.substr(8) + "&openID=" + req.session["openid"];
+            }
+            else {
+                console.log('rewrite to' + (path.substr(8) + "?openID=" + req.session["openid"]));
+                return path.substr(8) + "?openID=" + req.session["openid"];
+            }
         }
     },
     onProxyReq: function (proxyReq, req, res) {
--- app.ts
+++ app.ts
@@ -89,8 +89,14 @@ app.use(proxy('/service', {
     target: config.serverUrl, 
     changeOrigin: true,
     pathRewrite: function(path: string, req){
-        if (path.indexOf('/serivce') == 0) {
-            return `${path.substr(8)}&openID=${req.session["openid"]}`
+        if (path.indexOf('/service') == 0) {
+            console.log('path is ' + path)
+            if (path.indexOf('?') >= 0) {
+                return `${path.substr(8)}&openID=${req.session["openid"]}`
+            } else {
+                console.log('rewrite to' + `${path.substr(8)}?openID=${req.session["openid"]}`)
+                return `${path.substr(8)}?openID=${req.session["openid"]}`
+            }
         }
     },
     onProxyReq: (proxyReq, req, res) => {
